ce7765fd981a8af3240bb2aa51df286ece5191ba
Jost <jost.reelsen@gmx.de>
2025-04-02 18:42:18 +0200

    Update connect_noco.py

--- backend/api/connect_noco.py
+++ backend/api/connect_noco.py
@@ -16,42 +16,8 @@ xc_token = os.getenv("xc_token") # NocoDB Tokne - Denis Herri
 server = os.getenv("server") # START Munich DB Server
 table_id = os.getenv("table_id") # Members Table ID
 
-
-class Record(BaseModel):
-    Name: str
-    Last_modified: str
-    E_Mail: str
-    Membership: str
-    Department: str
-    Batch: str
-    City: str
-    Pronouns: str
-    Birthday: str
-    LinkedIn: str
-    Nationality: str
-    Study_Program: str
-    University: str
-    Study_Level: str
-    Private_E_Mail: str
-
-# Sample JSON data
-sample_record_json = {
-    "Name": "John Doe",
-    "Last_modified": "2023-10-01 15:45:00+00:00",
-    "E_Mail": "john.doe@startmunich.de",
-    "Membership": "Active",
-    "Department": "Marketing",
-    "Batch": "WS23",
-    "City": "Munich",
-    "Pronouns": "they/them",
-    "Birthday": "1998-07-20",
-    "LinkedIn": "https://www.linkedin.com/in/johndoe/",
-    "Nationality": "American",
-    "Study_Program": "Business Administration",
-    "University": "Ludwig Maximilian University of Munich",
-    "Study_Level": "Master",
-    "Private_E_Mail": "john.doe@example.com"
-}
+class Data(BaseModel):
+    text: str
 
 @app.get("/get_records")
 # Read all records from a table.
@@ -67,31 +33,14 @@ async def get_all_records():
 
 @app.post("/create_record")
 # Create a new record in a table.
-def create_record(record: Record = Body(...)):
+def create_record(data: Data = Body(...)):
     api_call = f"{server}/api/v2/tables/{table_id}/records"
+    print(data)
     response = requests.post(
         api_call,
         headers={'xc-token': xc_token},
-        json=record.dict()
+        json=data.dict()
     )
     response.raise_for_status()
     print(json.dumps(response.json(), indent=4))
     return response.json()
-
-# sample_record = Record(**{
-#     "Name": "tester 1",
-#     "Last_modified": "2024-06-17 12:32:00+00:00",
-#     "E_Mail": "sample_user@startmunich.de",
-#     "Membership": "Active",
-#     "Department": "Finance & Operations",
-#     "Batch": "SS24",
-#     "City": "Munich",
-#     "Pronouns": "he/him",
-#     "Birthday": "2003-11-15",
-#     "LinkedIn": "https://www.linkedin.com/in/sample_user/",
-#     "Nationality": "German",
-#     "Study_Program": "Computer Science",
-#     "University": "Technical University of Munich",
-#     "Study_Level": "Bachelor",
-#     "Private_E_Mail": "sample_user@personal.com"
-# })
