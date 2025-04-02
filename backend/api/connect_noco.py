802a46b0f63a7c188dace0ae11550d5569ada4c8
Jost <jost.reelsen@gmx.de>
2025-04-03 00:31:32 +0200

    stable -- 03.04.

--- backend/api/connect_noco.py
+++ backend/api/connect_noco.py
@@ -34,7 +34,10 @@ async def get_all_records():
 @app.post("/create_record")
 # Create a new record in a table.
 def create_record(data: Data = Body(...)):
-    entries = data['data'].split(';')
+
+    entries = data.text.split(';')
+    for i in range(len(entries)):
+        print(entries[i])
     
     def rearrange_date(date_str):
         month_map = {
@@ -42,38 +45,48 @@ def create_record(data: Data = Body(...)):
             "May": "05", "Jun": "06", "Jul": "07", "Aug": "08",
             "Sep": "09", "Oct": "10", "Nov": "11", "Dec": "12"
         }
-        month_num = month_map[[date_str][4:7]]
-        day = date_str[9:11]
-        year = date_str[12:16]
-        time = date_str[17:25]
+        month_num = month_map[date_str[4:7]]
+        day = date_str[8:10]
+        year = date_str[11:15]
+        time = date_str[16:24]
         return f"{year}-{month_num}-{day} {time}+00:00"
+
+    def birthday(date_str):
+        month_map = {
+            "Jan": "01", "Feb": "02", "Mar": "03", "Apr": "04",
+            "May": "05", "Jun": "06", "Jul": "07", "Aug": "08",
+            "Sep": "09", "Oct": "10", "Nov": "11", "Dec": "12"
+        }
+        month_num = month_map[date_str[4:7]]
+        day = date_str[8:10]
+        year = date_str[11:15]
+        return f"{year}-{month_num}-{day}"
 
     fields = ["Name", "Last modified", "E-Mail", "Membership", "Department", "Batch", "City", "Pronouns", "Birthday", "LinkedIn", "Nationality", "Study Program", "University", "Study Level", "Private E-Mail"]
     
-    entries_formatted = [""] * len(fields)
+    entries_formatted = [""] * (len(fields) - 1)
     entries_formatted[0] = entries[0]
     entries_formatted[1] = rearrange_date(entries[1])
-    entries_formatted[2] = entries[2]
-    entries_formatted[3] = entries[3]
-    entries_formatted[4] = entries[4]
-    entries_formatted[5] = entries[5][1:]
-    entries_formatted[6] = "SS" if entries[6] == "Summersemester" else "WS" if entries[6] == "Wintersemester" else entries[6] + entries[7]
+    entries_formatted[2:4] = entries[2:4]
+    entries_formatted[4] = entries[4][2:]
+    entries_formatted[5] = ("SS" if entries[5] == "Summersemester" else "WS" if entries[5] == "Wintersemester" else entries[5]) + entries[6][2:]
+    entries_formatted[6] = entries[7]
     entries_formatted[7] = entries[8]
-    entries_formatted[8] = entries[9]
-    entries_formatted[9] = rearrange_date(entries[11])
-    entries_formatted[10] = entries[12]
-    entries_formatted[11] = entries[13]
-    entries_formatted[12] = entries[14]
-    entries_formatted[13] = entries[15]
-    entries_formatted[14] = entries[16]
-    entries_formatted[15] = entries[17]
+    entries_formatted[8] = birthday(entries[9])
+    entries_formatted[9:14] = entries[10:16]
+
+    db_row = dict(zip(fields, entries_formatted))
+
+    print(json.dumps(db_row, indent=4))
 
     api_call = f"{server}/api/v2/tables/{table_id}/records"
-    print(data)
+
     response = requests.post(
         api_call,
         headers={'xc-token': xc_token},
-        json=entries_formatted.dict()
+
+        #convert to json
+        json=db_row
     )
     response.raise_for_status()
     print(json.dumps(response.json(), indent=4))
